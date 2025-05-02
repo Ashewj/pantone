55e3fca79a8d367fb85adcda6ba3a39fc5bd1903
Ashewj <120068931+Ashewj@users.noreply.github.com>
2025-05-01 22:00:35 -0300

    Add files via upload

--- test2.py
+++ test2.py
@@ -1,21 +1,14 @@
-import pandas as pd
+from pywinauto import Application
 
-# Data provided
-data = [
-['PROCESS WHITE C', 'PRETO BLACK C', 'PROCESS YELLOW C', '130 C', 'PROCESS MAGENTA C', '187 C', 'VAZIO', 'VAZIO'],
-['PROCESS WHITE C', 'PRETO BLACK C', 'PROCESS YELLOW C', 'VAZIO', 'PROCESS MAGENTA C', '187 C', 'VAZIO', 'VAZIO'],
-['PROCESS WHITE C', 'PRETO BLACK C', '871 C', 'VAZIO', 'VAZIO', 'VAZIO', 'VAZIO', 'VAZIO'],
-['PROCESS WHITE C', 'PRETO BLACK C', 'PROCESS YELLOW C', 'REFLEX BLUE C', 'PROCESS MAGENTA C', 'VAZIO', 'VAZIO', 'PROCESS CYAN C'],
-['PROCESS WHITE C', 'VAZIO', 'PROCESS YELLOW C', 'REFLEX BLUE C', '485 C', 'VAZIO', 'VAZIO', 'VAZIO'],
-['PROCESS WHITE C', 'PRETO BLACK C', '116 C', 'REFLEX BLUE C', '1795 C', 'VAZIO', 'VAZIO', 'VAZIO'],
-['PROCESS WHITE C', 'PRETO BLACK C', '116 C', '4146 C', 'VAZIO', 'VAZIO', 'VAZIO', 'VAZIO'],
-['PROCESS WHITE C', 'PRETO BLACK C', 'PROCESS YELLOW C', 'VAZIO', 'PROCESS MAGENTA C', 'VAZIO', 'VAZIO', 'PROCESS CYAN C'],
-['PROCESS WHITE C', 'VAZIO', 'VAZIO', 'REFLEX BLUE C', '485 C', 'VAZIO', 'VAZIO', 'VAZIO'],
-['PROCESS WHITE C', 'PRETO BLACK C', 'PROCESS YELLOW C', '7509 C', 'PROCESS MAGENTA C', 'VAZIO', 'VAZIO', 'PROCESS CYAN C'],
-]
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, module="pywinauto")
 
-# print(f"{grid[row]},")
-df = pd.DataFrame(data, columns=['cor1', 'cor2', 'cor3', 'cor4', 'cor5', 'cor6', 'cor7', 'cor8'])
-
-# print(f"{grid[row]},")
-df.to_excel('C:/Users/Administrator/Downloads/cores_ajustadas_separadas.xlsx', index=False)
+# Connect to Calculator
+app = Application(backend="win32").connect(path="calc.exe")
+calc = app.window(title="Calculator")
+for ctrl in calc.children():
+    print(ctrl)
+    if ctrl.friendly_class_name() == "Static":
+        text = ctrl.window_text()
+        if text.strip():
+            print("Found result:", text)
